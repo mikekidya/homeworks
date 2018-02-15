9b3c847f9bb6aa057402ad2a235e1073f4dcd43d
Mikhail Kidyankin <kidyankin@icloud.com>
2018-02-15 18:44:44 +0300

    codestyle fixed

--- Semester 2/1/1/src/group144/kidyankin/Main.java
+++ Semester 2/1/1/src/group144/kidyankin/Main.java
@@ -3,10 +3,10 @@ package group144.kidyankin;
 public class Main {
 
     public static void main(String[] args) {
-	    Stack stack = new Stack();
-	    stack.add(1);
-	    stack.add(4);
-	    stack.add(9);
+        Stack stack = new Stack();
+        stack.add(1);
+        stack.add(4);
+        stack.add(9);
         System.out.println(stack.getSize());
         stack.pop();
         System.out.println(stack.pop());
