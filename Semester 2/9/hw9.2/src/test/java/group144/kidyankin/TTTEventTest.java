003812240e9037b4b0dd8426e41535de7278949c
Mikhail Kidyankin <kidyankin@icloud.com>
2018-06-11 12:48:53 +0300

    hw9.1 added

---
+++ Semester 2/9/hw9.2/src/test/java/group144/kidyankin/TTTEventTest.java
@@ -0,0 +1,38 @@
+package group144.kidyankin;
+
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.junit.Assert.*;
+
+public class TTTEventTest {
+
+    @Test
+    public void sendTest() throws IOException, ClassNotFoundException {
+        TTTEvent tttEvent = new TTTEvent(new TTTEvent.ExitGameClickAction(), Tictactoe.Player.X, TTTEvent.ActionType.EXIT_GAME);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        tttEvent.send(objectOutputStream);
+
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+        ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+        TTTEvent received = (TTTEvent) objectInputStream.readObject();
+
+        assertEquals(received.getActionType(), tttEvent.getActionType());
+    }
+
+    @Test
+    public void getConstructorTest() throws IOException {
+        TTTEvent tttEvent = new TTTEvent(new TTTEvent.ExitGameClickAction(), Tictactoe.Player.X, TTTEvent.ActionType.EXIT_GAME);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        objectOutputStream.writeObject(tttEvent);
+
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+        ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+        TTTEvent received = new TTTEvent(objectInputStream);
+
+        assertEquals(received.getActionType(), tttEvent.getActionType());
+    }
+}
