3e0e8109550cd8e9bfced589a9ffa16932ce8d3d
Mikhail Kidyankin <kidyankin@icloud.com>
2018-12-12 20:37:34 +0300

    Some tabs removed 2

--- Semester 3/Tanks/desktop/src/group244/kidyankin/desktop/DesktopLauncher.java
+++ Semester 3/Tanks/desktop/src/group244/kidyankin/desktop/DesktopLauncher.java
@@ -8,12 +8,12 @@ import group244.kidyankin.TanksGame;
 
 /** Class realising method to launch game on desktop */
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		DesktopMini2DxConfig config = new DesktopMini2DxConfig(TanksGame.GAME_IDENTIFIER);
-		config.title = "Tank";
-		config.width = 800;
-		config.height = 600;
-		config.vSyncEnabled = true;
-		new DesktopMini2DxGame(new TanksGame(), config);
-	}
+    public static void main (String[] arg) {
+        DesktopMini2DxConfig config = new DesktopMini2DxConfig(TanksGame.GAME_IDENTIFIER);
+        config.title = "Tank";
+        config.width = 800;
+        config.height = 600;
+        config.vSyncEnabled = true;
+        new DesktopMini2DxGame(new TanksGame(), config);
+    }
 }
